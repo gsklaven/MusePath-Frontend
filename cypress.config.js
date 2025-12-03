17fa0a5327898766074d0224c1b2cd96278cd7d7
giorgosskl <sklavenitisgeorge21@gmail.com>
2025-12-03 18:39:18 +0200

    fix violations

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,5 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL, 
     
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 });
